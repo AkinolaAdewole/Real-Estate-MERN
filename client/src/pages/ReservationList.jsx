770d1e14da3e785ee6cd1e5a7ba1871557704d71
Akinola Adewole <adewoleakinola24@gmail.com>
2024-11-29 15:23:01 +0100

    server is ready

--- client/src/pages/ReservationList.jsx
+++ client/src/pages/ReservationList.jsx
@@ -17,7 +17,7 @@ const ReservationList = () => {
   const getReservationList = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/users/${userId}/reservations`,
+        `https://infinity-palace-akinolaadewole-akinola-adewoles-projects.vercel.app/users/${userId}/reservations`,
         {
           method: "GET",
         }
