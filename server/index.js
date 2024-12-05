1456cf11e1e92d391eb44d87a325405f3cfc144b
Akinola Adewole <adewoleakinola24@gmail.com>
2024-12-05 12:00:32 +0100

    render

--- server/index.js
+++ server/index.js
@@ -57,7 +57,8 @@ app.use("/properties", listingRoutes);
 app.use("/bookings", bookingRoutes); 
 app.use("/users", userRoutes); 
 
-const PORT = 3001; 
+const PORT = process.env.PORT || 3300;
+
 
 // Connect to the MongoDB database using Mongoose
 mongoose
